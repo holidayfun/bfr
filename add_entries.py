fc0a8a548461deafa980a5e2745aa69baff056e7
Joshua Hartmann <jhartmann@gmail.com>
2016-09-07 10:43:03 +0200

    PdCli connection

--- add_entries.py
+++ add_entries.py
@@ -5,13 +5,10 @@ sys.path.insert(0, '/home/hartmann/p4factory/cli')
 import pd_cli
 sys_path = 'tests/pd_thrift:../../testutils'
 
-
-
 sys.path.extend(sys_path.split(':'))
 
+cli = pd_cli.PdCli(p4_name='bfr', thrift_module='p4_pd_rcp.bfr', thrift_server='40.0.0.104', port = 22224)
 
 
-cli = pd_cli.PdCli(p4_name='bfr', thrift_module='pd_thrift', thrift_server='40.0.0.104', port = 22224)
-
-
-
+ip = '40.0.0.1'
+port = 22224
