66c118e4bb0780109da4e9922c8e3aadf9bd27ab
Joshua Hartmann <jhartmann@gmail.com>
2016-09-07 10:45:48 +0200

    sys path appended

--- add_entries.py
+++ add_entries.py
@@ -7,6 +7,15 @@ sys_path = 'tests/pd_thrift:../../testutils'
 
 sys.path.extend(sys_path.split(':'))
 
+sys_path = '../targets/bfr/tests/pd_thrift:../testutils'
+
+
+
+sys.path.extend(sys_path.split(':'))
+
+
+
+
 cli = pd_cli.PdCli(p4_name='bfr', thrift_module='p4_pd_rcp.bfr', thrift_server='40.0.0.104', port = 22224)
 
 
