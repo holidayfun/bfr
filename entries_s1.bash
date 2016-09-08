dee5eacc9bfdcdc905fbc3c6c651a7ff3d618625
Joshua Hartmann <jhartmann@gmail.com>
2016-09-08 10:49:56 +0200

    generated entries for s1

---
+++ entries_s1.bash
@@ -0,0 +1,3 @@
+python ../../cli/pd_cli.py -p bfr -i p4_pd_rpc.bfr -s $PWD/tests/pd_thrift:$PWD/../../testutils -m "add_entry ipv4_lpm 10.0.0.0 8 set_nhop 10.0.4.2" -c 40.0.0.102:22222
+python ../../cli/pd_cli.py -p bfr -i p4_pd_rpc.bfr -s $PWD/tests/pd_thrift:$PWD/../../testutils -m "add_entry send_frame 1 rewrite_mac 00:00:00:00:01:01" -c 40.0.0.102:22222
+python ../../cli/pd_cli.py -p bfr -i p4_pd_rpc.bfr -s $PWD/tests/pd_thrift:$PWD/../../testutils -m "add_entry forward 10.0.4.2 set_dmac 00:00:00:00:02:01" -c 40.0.0.102:22222
