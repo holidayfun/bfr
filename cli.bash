880838b870d7ad4b7bb7eb9c3b0dce29b8dd766f
holidayfun <jhartmann1995@gmail.com>
2016-09-09 13:20:53 +0200

    more comfort for cli

--- cli.bash
+++ cli.bash
@@ -1,5 +1,14 @@
 #startet eine interaktive CLI Instanz
-read -p "IP, auf die verbunden werden soll: " ip
-read -p "Thrift Port, auf den verbunden werden soll: " port
+#read -p "IP, auf die verbunden werden soll: " ip
+#read -p "Thrift Port, auf den verbunden werden soll: " port
+typeset -i sw_number
+typeset -i t
+read -p "Switch, auf den verbunden werden soll: " sw_number
+
+t=100+2*$sw_number
+ip="40.0.0.$t"
+port=22222
+
+echo "Verbinde zu $ip:$port"
 
 python ../../cli/pd_cli.py -p bfr -i p4_pd_rpc.bfr -s $PWD/tests/pd_thrift:$PWD/../../testutils -c $ip:$port "$@"
