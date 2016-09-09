153be5194b6f68811eb80c168eb955087eb49a69
holidayfun <jhartmann1995@gmail.com>
2016-09-09 16:17:38 +0200

    fixed typo

--- entries_s3.bash
+++ entries_s3.bash
@@ -7,4 +7,4 @@ python ../../cli/pd_cli.py -p bfr -i p4_pd_rpc.bfr -s $PWD/tests/pd_thrift:$PWD/
 python ../../cli/pd_cli.py -p bfr -i p4_pd_rpc.bfr -s $PWD/tests/pd_thrift:$PWD/../../testutils -m "add_entry send_frame 3 rewrite_mac 00:00:00:00:03:03" -c 40.0.0.106:22222
 python ../../cli/pd_cli.py -p bfr -i p4_pd_rpc.bfr -s $PWD/tests/pd_thrift:$PWD/../../testutils -m "add_entry forward 10.0.1.2 set_dmac 00:00:00:00:04:01" -c 40.0.0.106:22222
 python ../../cli/pd_cli.py -p bfr -i p4_pd_rpc.bfr -s $PWD/tests/pd_thrift:$PWD/../../testutils -m "add_entry forward 10.0.2.2 set_dmac 00:00:00:00:06:01" -c 40.0.0.106:22222
-python ../../cli/pd_cli.py -p bfr -i p4_pd_rpc.bfr -s $PWD/tests/pd_thrift:$PWD/../../testutils -m "add_entry forward 10.0.5.1 set_dmac 00:00:00:00:02:01" -c 40.0.0.106:22222
+python ../../cli/pd_cli.py -p bfr -i p4_pd_rpc.bfr -s $PWD/tests/pd_thrift:$PWD/../../testutils -m "add_entry forward 10.0.5.1 set_dmac 00:00:00:00:02:03" -c 40.0.0.106:22222
