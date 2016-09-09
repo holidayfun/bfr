a9a7a46a299517b1a885377107e3f630f49ac38f
holidayfun <jhartmann1995@gmail.com>
2016-09-09 13:00:03 +0200

    script to add all entries for s1-6

---
+++ all_entries.bash
@@ -0,0 +1,12 @@
+echo "entries for s1"
+bash entries_s1.bash
+echo "entries for s2"
+bash entries_s2.bash
+echo "entries for s3"
+bash entries_s3.bash
+echo "entries for s4"
+bash entries_s4.bash
+echo "entries for s5"
+bash entries_s5.bash
+echo "entries for s6"
+bash entries_s6.bash
