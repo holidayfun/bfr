b883f76827627b618f9735b279ca2fecb80c63f0
Joshua Hartmann <jhartmann1995@gmail.com>
2016-09-12 14:18:45 +0200

    added some more data

--- generateRingNetwork.py
+++ generateRingNetwork.py
@@ -12,6 +12,9 @@ for i in range(1,num_switches + 1):
             'number': i,
             'ip': '10.0.{0}.1'.format(i),
             'mac' : '00:00:00:00:{0:02d}:1'.format(i),
+            'control_network_ip' : '100.0.0.%s' % (100 + 2 * i),
+            'subnet_ip' : '10.0.{0}.0'.format(i),
+            'prefix_len' : 24,
             'hosts' : []
             }
 
@@ -32,11 +35,17 @@ for i in range(1,num_switches + 1):
 switch_links = []
 #generate inter-switch links
 for i in range(0,num_switches):
-    switch_links.append({ 'node1': network['switches'][i]['name'],
-                                'node2': network['switches'][(i+1) % num_switches]['name']})
+    switch_links.append({ 'node1': {'name' : network['switches'][i]['name'],
+                                    'ip' : '20.0.0.{0}'.format(2 * i + 1),
+                                    'mac' : '00:dd:00:00:00:{0:02d}'.format(2 * i + 1)},
+                          'node2': {'name' : network['switches'][(i+1) % num_switches]['name'],
+                                     'ip' : '20.0.0.{0}'.format(2 * i + 2),
+                                     'mac' : '00:dd:00:00:00:{0:02d}'.format(2 * i + 2)}})
+
+#print(switch_links)
 
 network['switch_links'] = switch_links
-
+#print(json.dumps(switch_links, sort_keys=True, indent=4, separators=(',', ': ')))
 #print(json.dumps(inter_switch_links, sort_keys=True, indent=4, separators=(',', ': ')))
 #print(json.dumps(s, sort_keys=True, indent=4, separators=(',', ': ')))
 #print(links)
